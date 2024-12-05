781b4610166652b1d1dab12e2bec9b83f1d38a5c
Kevin Manceau <kevin.manceau@gmail.com>
2024-12-05 11:44:35 +0100

    Displaying all of a hero info to test it

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Hero;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $hero = Hero::with('weapon')->find(2);
+        return view('home', compact('hero'));
     }
 }
