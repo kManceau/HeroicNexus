03d8eb1e2e59bd66dc59592eb0f9060dd9eeec05
Kevin Manceau <kevin.manceau@gmail.com>
2024-12-05 12:22:11 +0100

    [PAGE] All cards on a row now have the same height

--- resources/views/hero/index.blade.php
+++ resources/views/hero/index.blade.php
@@ -4,11 +4,13 @@
     <div class="container">
         <div class="row">
             @foreach($heroes as $hero)
-                <div class="col-md-3 mb-4">
-                    <div class="card shadow-lg" style="width: 100%; border-radius: 15px; overflow: hidden;">
-                        <div class="card-body">
+                <div class="col-md-3 mb-4 d-flex align-items-stretch">
+                    <div class="card shadow-lg w-100" style="border-radius: 15px; overflow: hidden;">
+                        <div class="card-body d-flex flex-column">
                             <h5 class="card-title text-primary">{{ $hero->name }}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">{{ $hero->race }} @if($hero->gender)| {{ $hero->gender }}@endif</h6>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $hero->race }} @if($hero->gender)
+                                    | {{ $hero->gender }}
+                                @endif</h6>
                             <hr>
                             <p class="card-text">
                                 <strong>Universe:</strong> {{ $hero->faction->universe->name }}<br>
@@ -27,14 +29,13 @@
                                 @endif
                             </p>
                             <hr>
-                            <p class="card-text">{{ $hero->description }}</p>
+                            <p class="card-text mt-auto">{{ $hero->description }}</p>
                         </div>
                     </div>
                 </div>
             @endforeach
         </div>
     </div>
-
 @endsection
 
 
